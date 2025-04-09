2874cd169daf095ff068f15558bff49dd433bab6
sasha <sashasamanuik@gmail.com>
2025-04-09 16:45:14 +0300

    в

---
+++ app/src/main/java/com/example/myproject/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonFirst = findViewById<Button>(R.id.buttonFirst)
+        val buttonSecond = findViewById<Button>(R.id.buttonSecond)
+
+        buttonFirst.setOnClickListener {
+            val intent = Intent(this, FirstActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonSecond.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
